35dd04178e0319d9f9d273f55537d6ab689b2f08
Zijie Li <michael20001122@gmail.com>
2024-05-03 23:58:51 -0400

    chore: Update npm dependencies and amplify configuration

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cloudcomputing3136dd52": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports';
+// import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Button } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Authenticator>
+          {({ signOut, user }) => (
+            <>
+              <h2>Welcome, {user.username}</h2>
+              <Button onClick={signOut}>Sign out</Button>
+            </>
+          )}
+        </Authenticator>
       </header>
     </div>
   );
 }
 
-export default App;
+export default (App);
